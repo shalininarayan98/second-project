e0d95c352715601c2d47b5e5a0649d5026b2aeb0
Your Name <you@example.com>
2020-01-02 19:47:00 +0000

    updates to services

---
+++ service4/application/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = ('mysql+pymysql://' + getenv('MY_SQL_USER') + ':' + getenv('MY_SQL_PASSWORD') + '@' + getenv('MY_SQL_URL') + '/' + getenv('MY_SQL_DB'))
+
+
+app.config['SECRET_KEY'] = getenv('SECRET_KEY')
+
+
+db = SQLAlchemy (app)
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+
+
+from application import routes
+
