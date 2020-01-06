47b51b96fc964c7f35e238eab84a7274c26eb59d
Your Name <you@example.com>
2020-01-06 12:42:31 +0000

    json added, modified S1+2

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -2,10 +2,15 @@ import numpy as np
 from random import randint
 from application import app
 from flask import render_template
-
+import requests
 
 @app.route('/', methods=['GET', 'POST'])
 def random_number():
     random_number=randint(1,10)
 
     return random_number
+
+response=requests.get("http://localhost:5000")
+print(response.text)
+
+requests.post("http://localhost:5000", json={random_number}
--- service2/application/routes.py
+++ service2/application/routes.py
@@ -1,9 +1,15 @@
 import random
 import string
+import requests
 
 @app.route('/', methods=['GET', 'POST'])
-def random_letter():
-    string.ascii_letters
-    random.choice(string.ascii_letters[0:4])
 
+def random_letter():
+    upper_alphabet = string.ascii_uppercase
+    random_letter = random.choice(upper_alphabet[0:6])
     return random_letter
+
+response=requests.get("http://localhost:5000")
+print(response.text)
+
+requests.post("http://localhost:5000", json={random_letter}
