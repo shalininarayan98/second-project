64fc612efd49bde850342ffebc572ecade5238fb
Your Name <you@example.com>
2020-01-13 05:42:43 +0000

    pytest

---
+++ service3/testing/predictions_test.py
@@ -0,0 +1,6 @@
+import pytest
+from application.routes import prediction
+
+def prediction_test():
+    response = self.client.get(url("http://service3:5003")
+    self.assertEqual(response.status_code, 200)
